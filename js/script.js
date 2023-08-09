b92bc185e80632bcd6697560fb5704e76273f5bf
screwax <screwaxx@gmail.com>
2023-08-09 15:44:41 +0300

    1

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const result = 5 + 5+ '5';
+console.log(result);
+
+const email = "screwaxx@gmail.com";
+console.log(""+email+"; "+String(email.includes("@"))+"; "+String(email.length)+"");
+
+const My = "My";
+const name = "name";
+const is = "is";
+let fullName = My + ' ' + name + ' ' + is;
+fullName += ' Viktor';
+console.log(fullName);
+
+const userName = "Danil";
+const payment = 300;
+alert("Дякуємо, "+userName+"! До сплати "+payment+" гривень");
